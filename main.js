7c4f99bace9595b55c94e233b7acacf7d0845be0
vimllaa <vilma.ohrman@hyperisland.se>
2021-11-08 13:40:23 +0100

    starting with js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const container = document.querySelector(".container");
-left.addEventListener("mouseenter", () => {
-  container.classList.add("hover-left");
-});
-left.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-left");
-});
-right.addEventListener("mouseenter", () => {
-  container.classList.add("hover-right");
-});
-right.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-right");
+let btnRandom = document.querySelector("button");
+let result = document.querySelector("h1");
+
+let checkIns = [
+  "What’s one thing that brings you energy and joy?",
+  "What’s become apparent since we last met?",
+  "Given our work so far, what do you feel best about?",
+  "What are you noticing in your environment that relates to this project?",
+];
+
+function getRandomNumber(min, max) {
+  let step1 = max - min + 1;
+  let step2 = Math.random() * step1;
+  let result = Math.floor(step2) + min;
+  return result;
+}
+
+btnRandom.addEventListener("click", () => {
+  let index = getRandomNumber(0, checkIns.length - 1);
+  result.innerText = checkIns[index];
 });
