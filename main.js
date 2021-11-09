2b86176c81a1703a0b54fd0767d9c7eed8113883
vimllaa <vilma.ohrman@hyperisland.se>
2021-11-09 15:56:53 +0100

    filter step 2
    mostly javascript and added some buttons

--- main.js
+++ main.js
@@ -1,23 +1,127 @@
-var button = document.getElementById("generate");
-var result = document.querySelector("h2");
+// ------ Declaring varibles ------
+let btnRandom = document.getElementById("generateButton");
+let result = document.getElementById("Check-in-text");
+let filterBtn = document.getElementById("filterbutton");
 
-//------- Buttons -------
+let filters = [];
 
-// ------ check-in questions ------
-var checkIns = [
-  "What’s one thing that brings you energy and joy?",
-  "What’s become apparent since we last met?",
-  "Given our work so far, what do you feel best about?",
-  "What are you noticing in your environment that relates to this project?",
-  "What color represents me and my personality?",
-  "What’s the best piece of advice you’ve ever been given?",
-  "What is your favorite item you’ve bought this year?",
+let connectionClicked = false;
+let funClicked = false;
+let workClicked = false;
+
+// ------ Check-in questions ------
+
+let activities = [
+  {
+    name: "What’s one thing that brings you energy and joy",
+    tags: ["fun", "work", "connection"],
+  },
+
+  {
+    name: "Fun Work & Connection",
+    tags: ["fun", "work", "connection"],
+  },
+
+  {
+    name: "What’s become apparent since we last met?",
+    tags: ["work"],
+  },
+
+  {
+    name: "This is work related",
+    tags: ["work"],
+  },
+  {
+    name: "What color represents me and my personality?",
+    tags: ["fun"],
+  },
+  {
+    name: "What is your favorite item you’ve bought this year?",
+    tags: ["connection"],
+  },
+  {
+    name: "Hello?",
+    tags: ["connection"],
+  },
+  {
+    name: "Testing",
+    tags: ["connection"],
+  },
+  {
+    name: "What fruit are you????",
+    tags: ["connection"],
+  },
 ];
 
-//  ----- function to randomize and generate questions -----
-function generate_question() {
-  random_index = Math.floor(Math.random() * checkIns.length);
+const connectionQuestion = activities.filter((x) =>
+  x.tags.includes("connection")
+);
+const funQuestion = activities.filter((x) => x.tags.includes("fun"));
+const workQuestion = activities.filter((x) => x.tags.includes("work"));
 
-  selected_check_in = checkIns[random_index];
-  document.getElementById("MyText").innerHTML = selected_check_in;
+function getRandomNumber(min, max) {
+  let step1 = max - min + 1;
+  let step2 = Math.random() * step1;
+  let result = Math.floor(step2) + min;
+
+  return result;
 }
+
+btnRandom.addEventListener("click", () => {
+  let availableActivities = activities;
+
+  console.log(availableActivities);
+  console.log(filters);
+
+  filters.forEach((filter) => {
+    availableActivities = availableActivities.filter((x) =>
+      x.tags.includes(filter)
+    );
+  });
+
+  if (availableActivities.length > 0) {
+    let index = getRandomNumber(0, availableActivities.length - 1);
+    console.log("hello", result);
+    result.innerText = availableActivities[index].name;
+
+    console.log("ACTIVITY", availableActivities[index].name);
+  }
+});
+
+let work = document.getElementById("work");
+work.addEventListener("click", function () {
+  if (!workClicked) {
+    filters.push("work");
+  } else {
+    filters = filters.filter((x) => x != "work");
+  }
+  work = !workClicked;
+});
+
+let fun = document.getElementById("fun");
+fun.addEventListener("click", function () {
+  if (!funClicked) {
+    filters.push("fun");
+  } else {
+    filters = filters.filter((x) => x != "fun");
+  }
+
+  funClicked = !funClicked;
+});
+
+let connection = document.getElementById("connection");
+connection.addEventListener("click", function () {
+  if (!connectionClicked) {
+    filters.push("connection");
+  } else {
+    filters = filters.filter((x) => x != "connection");
+  }
+
+  connectionClicked = !connectionClicked;
+});
+
+// document.getElementById("savebutton").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   document.querySelector(".modal-content").style.display = "none";
+//   document.getElementById("everythingbutmodal").style.opacity = "1";
+// });
