74e78187bf0435669a743f5e081061c28ca28102
Daria Koltcova <daria.koltcova@hyperisland.se>
2021-11-10 09:19:26 +0100

    modal

--- main.js
+++ main.js
@@ -140,3 +140,21 @@ connection.addEventListener("click", function () {
 
   connectionClicked = !connectionClicked;
 });
+
+// Modal-window-----
+const modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function(trigger) {
+  trigger.addEventListener('click', function(event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add('open');
+    const exits = modal.querySelectorAll('.modal-exit');
+    exits.forEach(function(exit) {
+      exit.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.classList.remove('open');
+      });
+    });
+  });
+});
